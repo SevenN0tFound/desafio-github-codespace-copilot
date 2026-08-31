771fe3a214c9295a8d77d86479597f4a4aa5df61
Bernardo Ferreira <160356148+SevenN0tFound@users.noreply.github.com>
2026-08-31 05:53:48 +0000

    Fazendo o inicio do codigo com copilot

---
+++ concat_dados.py
@@ -0,0 +1,10 @@
+#Vamos receber dois dados diferentes do usuário e concatena-los
+
+info1 = input("Digite o primeiro dado: ")
+info2 = input("Digite o segundo dado: ")
+
+#Concatenando os dados
+resultado = info1 + " " + info2
+
+#Exibindo o resultado
+print("O resultado da concatenação é:", resultado)
