c5eecdbf9157f0c828de6dcf4725dc97091738c1
Bernardo Ferreira <160356148+SevenN0tFound@users.noreply.github.com>
2026-08-31 06:49:36 +0000

    Criando um codigo que calcula a media de 3 notas

---
+++ resolucoes_code/med_not.py
@@ -0,0 +1,13 @@
+# Calcular a média de três notas fornecidas na entrada do usuário.
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+
+if media < 0:
+    media = 0
+elif media > 10:
+    media = 10
+
+print(f"A média das notas é: {media:.2f}")
